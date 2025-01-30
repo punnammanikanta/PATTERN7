cb943a8e63fa259afc67c52b2c6a188856e0bb2c
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 13:26:37 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,24 @@
+public class Pattern7 {
+    public static void main(String[]args){
+        int n =4 ;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            if(i==1&&j==3){
+                System.out.print("*");
+            }else if (i==2 &&(j==2||j==4)){
+                System.out.print("*");
+            }else if (i==3 &&(j==1||j==5)){
+                System.out.print("*");
+            }else if (i==4&&(j==2||j==4)){
+                System.out.print("*");
+            }else if (i==5&&j==3){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println(" ");
+    }
+    
+}
+}
